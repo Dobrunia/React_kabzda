ffdfba6af793310297a8d6203140a50a6873f68d
Dobrunia <dobkos@hotmail.com>
2023-03-23 18:14:07 +0300

    2

--- src/index.tsx
+++ src/index.tsx
@@ -17,3 +17,15 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// let users : Array<object> = [
+//   {
+//     name: "Dobrunia"
+//   },
+//   {
+//     name: "Alex"
+//   }
+// ];
+
+// console.log(users[0].name)
