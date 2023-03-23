2acd2029906d089185a0cffb8289189f29b4ecbf
Dobrunia <dobkos@hotmail.com>
2023-03-23 19:11:23 +0300

    props tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Accordion from './components/Accordion/Accordion';
 
 function App() {
   return (
     <div>
-      <AddTitle/>
-      <Rating value={3}/>
-      <Accordion/>
+      <AddTitle />
+      <Rating value={3} />
+      <Accordion titleValue={"Menu"} />
     </div>
-    
+
   );
 }
 
-function Rating(props : any) {
+type RatingPropsType = {
+  value: 0 | 1 | 2 | 3 | 4 | 5;
+}
+
+function Rating(props: RatingPropsType) {
   if (props.value === 0) {
     return (
       <div>
-        <Star selected={false}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
+        <Star selected={false} />
+        <Star selected={false} />
+        <Star selected={false} />
+        <Star selected={false} />
+        <Star selected={false} />
       </div>
-      
+
     );
   };
   if (props.value === 1) {
     return (
       <div>
-        <Star selected={true}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
+        <Star selected={true} />
+        <Star selected={false} />
+        <Star selected={false} />
+        <Star selected={false} />
+        <Star selected={false} />
       </div>
-      
+
     );
   }
   if (props.value === 2) {
     return (
       <div>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
+        <Star selected={true} />
+        <Star selected={true} />
+        <Star selected={false} />
+        <Star selected={false} />
+        <Star selected={false} />
       </div>
-      
+
     );
   }
   if (props.value === 3) {
     return (
       <div>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={false}/>
-        <Star selected={false}/>
+        <Star selected={true} />
+        <Star selected={true} />
+        <Star selected={true} />
+        <Star selected={false} />
+        <Star selected={false} />
       </div>
-      
+
     );
   }
   if (props.value === 4) {
     return (
       <div>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={false}/>
+        <Star selected={true} />
+        <Star selected={true} />
+        <Star selected={true} />
+        <Star selected={true} />
+        <Star selected={false} />
       </div>
-      
+
     );
   }
   if (props.value === 5) {
     return (
       <div>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={true}/>
-        <Star selected={true}/>
+        <Star selected={true} />
+        <Star selected={true} />
+        <Star selected={true} />
+        <Star selected={true} />
+        <Star selected={true} />
       </div>
     );
   }
   return <div>ОПА</div>
 }
 
-function Star(props : any) {
+type StartPropsType = {
+  selected: boolean;
+}
+
+function Star(props: StartPropsType) {
   if (props.selected === true) {
     return <span><b>star </b></span>;
   } else {
     return <span>star </span>;
   }
-  
+
 }
 
 function AddTitle() {
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 
-function Accordion() {
-    return (
-    <div>
-      <AccordionTitle/>
-      <AccordionBody/>
-    </div>
-    );
-  }
-  
-  function AccordionTitle() {
-    return <h3>Menu</h3>
-  }
-  
-  function AccordionBody() {
-    return <>
-      <ul>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-      </ul>
-    </>
-  }
+type AccordionPropsType = {
+    titleValue: string;
+}
 
-  export default Accordion;
+function Accordion(props: AccordionPropsType) {
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue} />
+            <AccordionBody />
+        </div>
+    );
+}
+
+type AccordionTitlePropsType = {
+    title: string;
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return <h3>{props.title}</h3>
+}
+
+function AccordionBody() {
+    return <>
+        <ul>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+        </ul>
+    </>
+}
+
+export default Accordion;
